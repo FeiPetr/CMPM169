e9054d2b364f8ac3ffd6595d8c0371bea683d829
FeiPetr <777spetrova@gmail.com>
2024-02-03 23:45:32 -0800

    adding experiment

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,140 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Sofia Petrova
+// Date: February 3rd 2024
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let song;
+let img;
+let creepyimg;
 
-// Globals
-let myInstance;
-let canvasContainer;
+var slider;
+var pixelSizeSlide;
+var oldSlider;
+var imgReset;
+var addChange;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
 
-    myMethod() {
-        // code to run when method is called
-    }
+function preload()
+{
+  song = loadSound('mac.mp3');
+  sadsong = loadSound('spooky.mp3');
+
+  img = loadImage('menufor4.png');
+  imgReset = loadImage('menufor4.png');
+
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(400, 400);
+  img.resize(width, height);
+  imgReset.resize(width, height);
+  pixelSizeSlide = 5;
+  oldSlider = pixelSizeSlide;
+  addChange = 0;
+
+  pixelDensity(1); // Set pixel density to 1 to ensure one pixel corresponds to one pixel in the canvas
+  noStroke();
+  image(imgReset, 0, 0); // Display the original image
+
+  image(img, 0, 0); // Display the original image
+  loadPixels();
+  pixelate(pixelSizeSlide); // Adjust the pixel size as needed
+  song.setVolume(1);
+  slider = createSlider(0.1, 1, 0.1, 0.1);
+    song.play();
+
+
+
+
+
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+
+function pixelate(pixelSize) {
+  img.loadPixels(); //load pixels
+  //console.log(pixels);
+
+  for (let y = 0; y < height; y += pixelSize) {
+    for (let x = 0; x < width; x += pixelSize) {
+      let loc = (x + y * width) * 4; // Calculate the pixel location in the pixel array
+      // Get the average color of the pixel block
+      let avgColor = getAverageColor(x, y, pixelSize);
+      //console.log(avgColor);
+
+      // Apply the average color to each pixel in the block
+      for (let blockY = 0; blockY < pixelSize; blockY++) {
+        for (let blockX = 0; blockX < pixelSize; blockX++) {
+          let index = (x + blockX + (y + blockY) * width) * 4;
+          pixels[index] = avgColor[0];
+          pixels[index + 1] = avgColor[1];
+          pixels[index + 2] = avgColor[2];
+          pixels[index + 3] = avgColor[3];
+        }
+      }
+    }
+  }
+
+  updatePixels();
+}
+
+
+function getAverageColor(x, y, pixelSize) {
+  let totalR = 0,
+    totalG = 0,
+    totalB = 0,
+    totalA = 0;
+
+  for (let blockY = 0; blockY < pixelSize; blockY++) {
+    for (let blockX = 0; blockX < pixelSize; blockX++) {
+      let loc = (x + blockX + (y + blockY) * width) * 4;
+      //console.log(totalR);
+
+      totalR += pixels[loc];
+      totalG += pixels[loc + 1];
+      totalB += pixels[loc + 2];
+      totalA += pixels[loc + 3];
+    }
+  }
+
+  let avgR = totalR / (pixelSize * pixelSize);
+  let avgG = totalG / (pixelSize * pixelSize);
+  let avgB = totalB / (pixelSize * pixelSize);
+  let avgA = totalA / (pixelSize * pixelSize);
+
+  return [avgR, avgG, avgB, avgA];
+}
+
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  
+  song.setVolume(slider.value());
+  pixelSizeSlide = slider.value()*20; //update pixel size
+  if (oldSlider != pixelSizeSlide && addChange < 80)
+    {
+      img = imgReset;
+      img.loadPixels();
+      pixelate(pixelSizeSlide); // Adjust the pixel size as needed
+      oldSlider = pixelSizeSlide;
+      addChange++;
+      
+    }
+  
+  if(addChange>70)
+    {
+      background(0);
+      textSize(32);
+      fill(255);
+      text('jumpscare', 50, 50);
+      song.stop();
+      sadsong.play();
+      sadsong.setVolume(slider.value());
+    }
+    
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
