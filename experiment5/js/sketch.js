07f094e4def4df1b325a76e10bd199aadd92ba72
FeiPetr <777spetrova@gmail.com>
2024-02-12 22:02:47 -0800

    experiment 5

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,77 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let teapotModel;
+let timer = 10000000;
+let angle = 90;
+let mc;
+let mcUrl = 'https://i.imgur.com/YokgHOl.png'; // Replace 'https://example.com/image.jpg' with the URL of your image
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+function preload() {
+  teapotModel = loadModel('maxwell.obj', true);
+  img = loadImage('maxwellcors-2.png');
+  mc = loadImage(mcUrl);
 
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(500, 500, WEBGL);
+  noStroke(); // Disable stroke
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+
+  //debugMode();
+  describe('displays a model of a teapot using normalMaterial()');
+  rotateX(100)
+    //blendMode(SCREEN);
+
+
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+function draw(){
+  tint(255,255,153);
+  image(mc, -200, -200, width, height);
+    
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+  //ambientMaterial(250);
+  lights();
+  ambientLight(255,255,153); 
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+
+  //normalMaterial(); // For effect
+  //rotateY(90);
+  //rotateX(PI);
+  //if timer etc,
+  if(timer%2 >= 1)
+  {
+    rotateZ(angle);
+  }
+  else{
+    rotateZ(angle);
+  }
+
+  // Move the box side to side along the x-axis
+  /*let xOffset = sin(angle) * 100; // Adjust the amplitude as needed
+  //translate(xOffset, 0, 0);
+
+  // Draw the rotating box
+  rotateY(angle);*/
+  texture(img);
+  model(teapotModel); //draws model after transformations
+
+  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+    timer --;
+  }
+  
+    if(timer%2 >= 1)
+  {
+    angle -=0.05;
+  }
+  else{
+    angle +=0.05;
+  }
+
+  
+
+
+
+  
+
 }
